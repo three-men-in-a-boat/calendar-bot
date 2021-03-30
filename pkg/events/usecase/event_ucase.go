a4cf5e12da72609d43190c835098af2c4a873eea
esuwu <sanya554455@gmail.com>
2021-03-31 01:23:26 +0300

    tmp

--- pkg/events/usecase/event_ucase.go
+++ pkg/events/usecase/event_ucase.go
@@ -1,7 +1,6 @@
 package usecase
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/calendar-bot/pkg/events/repository"
@@ -164,70 +163,4 @@ func (uc *EventUseCase) GetEventsByDate(accessToken string, date time.Time) (*ty
 	return getEventsBySpecificDay(date, accessToken)
 }
 
-type EventInput struct {
-	Uid         string    `json:"uid,omitempty"`
-	Title       string    `json:"title,omitempty"`
-	From        string `json:"from,omitempty"`
-	To          string `json:"to,omitempty"`
-	FullDay     bool      `json:"fullDay,omitempty"`
-	Description string    `json:"description,omitempty"`
-	Location    types.Location  `json:"location,omitempty"`
-	Calendar    types.Calendar  `json:"calendar,omitempty"`
-	Attendees   types.Attendees `json:"attendees,omitempty"`
-	Call        string    `json:"call,omitempty"`
-	Chat		string 		`json:"chat,omitempty"`
-	Payload     string    `json:"payload,omitempty"`
-}
-
-type HTTPResponse struct {
-	statusCode int
-	response string
-}
-
-func (uc *EventUseCase) CreateEvent(accessToken string, eventInput EventInput) (*HTTPResponse, error) {
-	mutationReq := fmt.Sprintf(`mutation{createEvent(event: {uid: \"%s\", title: \"%s\", from: \"%s\", to: \"%s\", description: \"%s\"}) {uid}}`, eventInput.Uid, eventInput.Title, eventInput.From, eventInput.To,eventInput.Description)
-	eventCreationReq := fmt.Sprintf(`{"query":"%s"}`, mutationReq)
-
-	request, err := http.NewRequest("POST", "https://calendar.mail.ru/graphql", bytes.NewBuffer([]byte(eventCreationReq)))
-	if err != nil {
-		zap.S().Errorf("failed to create a request %s", err)
-		return nil, err
-	}
-
-	var bearerToken = "Bearer " + accessToken
-	request.Header.Add(
-		"Authorization",
-		bearerToken,
-	)
-	request.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{Timeout: time.Second * 10}
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, errors.Errorf("The HTTP request failed with error %v", err)
-	}
-	defer func() {
-		err = response.Body.Close()
-		if err != nil {
-			zap.S().Errorf("failed to close body of response of func getEvents, %v", err)
-		}
-	}()
-
-	res, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	eventsResponse := types.EventsResponse{}
-
-	err = json.Unmarshal(res, &eventsResponse)
-	if err != nil {
-		return nil, err
-	}
-	responseHTTP := HTTPResponse{}
-	responseHTTP.statusCode = response.StatusCode
-	responseHTTP.response = string(res)
-
-	return &responseHTTP, nil
-}
 
