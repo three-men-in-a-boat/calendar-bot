53a1d1c323460fa63b5e6f2961b589c07812bf60
Nikolay Eskov <mr.eskov1@yandex.ru>
2021-05-04 18:21:21 +0300

    New version of GetUsersFreeIntervals method in event usecase

--- pkg/events/usecase/freebusy.go
+++ pkg/events/usecase/freebusy.go
@@ -6,17 +6,57 @@ import (
 	"time"
 )
 
+type DayPart struct {
+	Start    time.Time
+	Duration time.Duration
+}
+
+// ------------span modifiers----
+
+func StretchOutSpan(span spaniel.Span, d time.Duration) spaniel.Span {
+	/* nickeskov:
+	for example: span is 12:11 - 12:47; d is 15minutes
+	start evaluates in 12:00
+	end evaluates in 13:00
+	*/
+	start := span.Start().Truncate(d)
+	end := span.End().Truncate(d)
+	// nickeskov: if end truncated, add d
+	if !end.Equal(span.End()) {
+		end = end.Add(d)
+	}
+	return spaniel.New(start, end)
+}
+
+func SplitSpanBy(span spaniel.Span, splitInterval time.Duration) (spanSplit spaniel.Spans, remainder spaniel.Span) {
+	spanSplit = spaniel.Spans{}
+	remainder = nil
+	for span.End().Sub(span.Start()) >= splitInterval {
+		nextStart := span.Start().Add(splitInterval)
+		spanSplit = append(spanSplit, spaniel.New(span.Start(), nextStart))
+		span = spaniel.New(nextStart, span.End())
+	}
+	if span.Start() != span.End() {
+		remainder = span
+	}
+	return spanSplit, remainder
+}
+
+// ------------span modifiers----
+
 // ------------filters------------
 
-type FilterFunc func(span spaniel.Span) bool
+type SpanFilterFunc func(span spaniel.Span) bool
 
 type FreeBusyConfig struct {
-	DayPartSpan             spaniel.Span
+	DayPart                 *DayPart
+	StretchBusyIntervalsBy  *time.Duration
+	SplitFreeIntervalsBy    *time.Duration
 	MinFreeIntervalDuration *time.Duration
 	MaxFreeIntervalDuration *time.Duration
 }
 
-func MergeSpanFilters(filters ...FilterFunc) FilterFunc {
+func MergeSpanFilters(filters ...SpanFilterFunc) SpanFilterFunc {
 	return func(span spaniel.Span) bool {
 		for _, filter := range filters {
 			if !filter(span) {
@@ -31,48 +71,17 @@ func NotInInterval(span, borders spaniel.Span) bool {
 	return borders.Start().After(span.Start()) || borders.End().Before(span.End())
 }
 
-func NotInDayPart(span, dayBorders spaniel.Span) bool {
-	spanStartHour, spanStartMinute, spanStartSecond := span.Start().Clock()
-	spanStartNanosecond := span.Start().Nanosecond()
-	spanStartLoc := span.Start().Location()
+func NotInDayPart(span spaniel.Span, part DayPart) bool {
+	hour, minute, second := part.Start.Clock()
+	nanosecond := part.Start.Nanosecond()
 
-	spanStart := time.Date(
-		0, 0, 0,
-		spanStartHour, spanStartMinute, spanStartSecond, spanStartNanosecond,
-		spanStartLoc,
-	)
+	year, month, day := span.Start().Date()
+	loc := span.Start().Location()
 
-	spanEndHour, spanEndMinute, spanEndSecond := span.End().Clock()
-	spanEndNanosecond := span.End().Nanosecond()
-	spanEndLoc := span.End().Location()
+	startBorder := time.Date(year, month, day, hour, minute, second, nanosecond, loc)
+	endBorder := startBorder.Add(part.Duration)
 
-	spanEnd := time.Date(
-		0, 0, 0,
-		spanEndHour, spanEndMinute, spanEndSecond, spanEndNanosecond,
-		spanEndLoc,
-	)
-
-	borderStartHour, borderStartMinute, borderStartSecond := dayBorders.Start().Clock()
-	borderStartNanosecond := dayBorders.Start().Nanosecond()
-	borderStartLoc := dayBorders.Start().Location()
-
-	startBorder := time.Date(
-		0, 0, 0,
-		borderStartHour, borderStartMinute, borderStartSecond, borderStartNanosecond,
-		borderStartLoc,
-	)
-
-	borderEndHour, borderEndMinute, borderEndSecond := dayBorders.End().Clock()
-	borderEndNanosecond := dayBorders.End().Nanosecond()
-	borderEndLoc := dayBorders.End().Location()
-
-	endBorder := time.Date(
-		0, 0, 0,
-		borderEndHour, borderEndMinute, borderEndSecond, borderEndNanosecond,
-		borderEndLoc,
-	)
-
-	return spanStart.Before(startBorder) || spanEnd.After(endBorder)
+	return span.Start().Before(startBorder) || span.End().After(endBorder)
 }
 
 func GreaterOrEqualThanDuration(span spaniel.Span, minDuration time.Duration) bool {
@@ -85,33 +94,31 @@ func LessOrEqualThanDuration(span spaniel.Span, maxDuration time.Duration) bool 
 	return spanDuration <= maxDuration
 }
 
-func FilterTimeSpansWithFunc(spans spaniel.Spans, filter FilterFunc) spaniel.Spans {
+func FilterSpansWithFunc(spans spaniel.Spans, filter SpanFilterFunc) spaniel.Spans {
 	filtered := make(spaniel.Spans, 0, len(spans))
-
 	for _, span := range spans {
 		if filter(span) {
 			filtered = append(filtered, span)
 		}
 	}
-
 	return filtered
 }
 
-func FilterTimeSpans(spans spaniel.Spans,
-	mainSpan spaniel.Span, dayPartSpan spaniel.Span,
+func FilterSpans(spans spaniel.Spans,
+	mainBordersSpan spaniel.Span, dayPart *DayPart,
 	minDuration *time.Duration, maxDuration *time.Duration) spaniel.Spans {
 
-	filters := make([]FilterFunc, 0)
+	filters := make([]SpanFilterFunc, 0)
 
-	if mainSpan != nil {
+	if mainBordersSpan != nil {
 		filters = append(filters, func(span spaniel.Span) bool {
-			return !NotInInterval(span, mainSpan)
+			return !NotInInterval(span, mainBordersSpan)
 		})
 	}
 
-	if dayPartSpan != nil {
+	if dayPart != nil {
 		filters = append(filters, func(span spaniel.Span) bool {
-			return !NotInDayPart(span, dayPartSpan)
+			return !NotInDayPart(span, *dayPart)
 		})
 	}
 
@@ -127,10 +134,49 @@ func FilterTimeSpans(spans spaniel.Spans,
 		})
 	}
 
-	return FilterTimeSpansWithFunc(spans, MergeSpanFilters(filters...))
+	return FilterSpansWithFunc(spans, MergeSpanFilters(filters...))
 }
 
 // ------------filters------------
+
+// ------------mappers------------
+
+type SpanMapFunc func(span spaniel.Span) spaniel.Span
+
+func TruncateSpanBy(borders spaniel.Span) SpanMapFunc {
+	return func(span spaniel.Span) spaniel.Span {
+		start := span.Start()
+		end := span.End()
+
+		if start.Before(borders.Start()) {
+			start = borders.Start()
+		}
+		if end.After(borders.End()) {
+			end = borders.End()
+		}
+
+		return spaniel.New(start, end)
+	}
+}
+
+func MergeSpanMappers(mappers ...SpanMapFunc) SpanMapFunc {
+	return func(span spaniel.Span) spaniel.Span {
+		for _, mapper := range mappers {
+			span = mapper(span)
+		}
+		return span
+	}
+}
+
+func MapSpansWithFunc(spans spaniel.Spans, mapper SpanMapFunc) spaniel.Spans {
+	mapped := make(spaniel.Spans, 0, len(spans))
+	for _, span := range spans {
+		mapped = append(mapped, mapper(span))
+	}
+	return mapped
+}
+
+// ------------mappers------------
 
 // ------------complements--------
 
@@ -152,10 +198,10 @@ func FlatComplementOfSpanComplementsRecursive(complements []spaniel.Spans) spani
 func FlatComplementOfSpanComplementsIterative(complements []spaniel.Spans) spaniel.Spans {
 	flatComplementOfSpanSet := spaniel.Spans{}
 	if len(complements) > 0 {
-		spanComplement := complements[0]
+		firstSpanComplement := complements[0]
+		flatComplementOfSpanSet = make(spaniel.Spans, len(firstSpanComplement))
+		copy(flatComplementOfSpanSet, firstSpanComplement)
 		complements = complements[1:]
-		flatComplementOfSpanSet = make(spaniel.Spans, len(spanComplement))
-		copy(flatComplementOfSpanSet, spanComplement)
 	}
 	for _, spanComplement := range complements {
 		flatComplementOfSpanSet = flatComplementOfSpanSet.IntersectionBetween(spanComplement)
@@ -163,9 +209,9 @@ func FlatComplementOfSpanComplementsIterative(complements []spaniel.Spans) spani
 	return flatComplementOfSpanSet
 }
 
-func CreateComplementForEachSpan(flatSpanSet spaniel.Spans, mainSpan spaniel.Span) []spaniel.Spans {
-	startMainInterval := mainSpan.Start()
-	endMainInterval := mainSpan.End()
+func CreateComplementForEachSpan(flatSpanSet spaniel.Spans, complementBorders spaniel.Span) []spaniel.Spans {
+	startMainInterval := complementBorders.Start()
+	endMainInterval := complementBorders.End()
 
 	complementsOfTimeSpans := make([]spaniel.Spans, 0, len(flatSpanSet))
 	for _, span := range flatSpanSet {
@@ -182,12 +228,38 @@ func CreateComplementForEachSpan(flatSpanSet spaniel.Spans, mainSpan spaniel.Spa
 
 // ------------complements--------
 
-func MergeBusyIntervals(freeBusyUser types.FreeBusyUser) spaniel.Spans {
+// ------------helpers------------
+
+func MergeBusyIntervals(freeBusyUser types.FreeBusyUser, stretchBy *time.Duration) spaniel.Spans {
 	busyTimeSpans := spaniel.Spans{}
 	for _, userSpans := range freeBusyUser.FreeBusy {
 		for _, span := range userSpans.FreeBusy {
-			busyTimeSpans = append(busyTimeSpans, spaniel.New(span.From, span.To))
+			var span spaniel.Span = span
+			if stretchBy != nil {
+				span = StretchOutSpan(span, *stretchBy)
+			}
+			busyTimeSpans = append(busyTimeSpans, span)
 		}
 	}
 	return busyTimeSpans.Union()
 }
+
+func CalculateFreeTimeSpansIterative(busy spaniel.Spans, complementBorders spaniel.Span) spaniel.Spans {
+	if len(busy) == 0 {
+		return spaniel.Spans{complementBorders}
+	}
+	complements := CreateComplementForEachSpan(busy, complementBorders)
+	freeTimeSpansIterative := FlatComplementOfSpanComplementsIterative(complements)
+	return freeTimeSpansIterative
+}
+
+func CalculateFreeTimeSpansRecursive(busy spaniel.Spans, complementBorders spaniel.Span) spaniel.Spans {
+	if len(busy) == 0 {
+		return spaniel.Spans{complementBorders}
+	}
+	complements := CreateComplementForEachSpan(busy, complementBorders)
+	freeTimeSpansRecursive := FlatComplementOfSpanComplementsRecursive(complements)
+	return freeTimeSpansRecursive
+}
+
+// ------------helpers------------
