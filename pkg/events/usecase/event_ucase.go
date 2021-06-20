016f0296337120b9385fab16fb29273c1f93992b
esuwu <sanya554455@gmail.com>
2021-06-20 18:22:16 +0300

    f

--- pkg/events/usecase/event_ucase.go
+++ pkg/events/usecase/event_ucase.go
@@ -472,14 +472,15 @@ func getJsonFromMap(m map[string]interface{}) string {
 	return response
 }
 
-func deleteIcs(filename string) error {
-	err := os.Remove(filename + ".ics")
-
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// TODO needs to be used
+//func deleteIcs(filename string) error {
+//	err := os.Remove(filename + ".ics")
+//
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
 
 func createICS(input types.EventInput, from time.Time, to time.Time) error {
 	cal := ics.NewCalendar()
